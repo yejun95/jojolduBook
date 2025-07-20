ccb538a1ea82b724cfe0be38cb52cc64ada6f5c8
jun <xodlf100@gmail.com>
2025-07-20 22:05:07 +0900

    step2 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -36,7 +36,7 @@ nohup java -jar \
   -Dspring.config.location=\
 classpath:/application.properties,\
 classpath:/application-real.properties,\
-/home/ec2-user/app/application-aouth.properties,\
+/home/ec2-user/app/application-oauth.properties,\
 /home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
