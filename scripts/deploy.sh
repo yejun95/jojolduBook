9baa48b67442578c27beb8079620d59a339f5a35
jun <xodlf100@gmail.com>
2025-07-20 22:37:02 +0900

    배포 자동화 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-# 수행 중인 애플리케이션 프로세스 ID => 구동 중이면 종료하기 위함
+# 수행 중인 애플리케이션 프로세스 ID => 구동 중이면 종료하기 위함.
 CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
