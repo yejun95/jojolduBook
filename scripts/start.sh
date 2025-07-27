ec18da9c6c6a282a0545fc1160b9f83404ff4f93
jun <xodlf100@gmail.com>
2025-07-27 21:50:08 +0900

    fix #10 배포 스크립트 수정 - 6

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,9 +14,9 @@ PROJECT_NAME=freelec-springboot2-webservice
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zIp/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
@@ -24,6 +24,8 @@ echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
