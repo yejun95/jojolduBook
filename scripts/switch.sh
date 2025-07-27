cd0cc5ba075036f16bf7724c0a05681c5e0e4d76
jun <xodlf100@gmail.com>
2025-07-27 22:02:28 +0900

    fix #10 배포 스크립트 수정 - 8

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -10,7 +10,7 @@ function switch_proxy() {
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 실행"
 
-    # Nginx 설정에서 사용할 #service_url 값을 지정하는 설정 문자 > 해당 내용을 service-url.inc 파일에 쓰기
+    # Nginx 설정에서 사용할 #service_url 값을 지정하는 설정 문자 > 해당 내용을 service-url.inc 파일에 쓴다.
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> Nginx reload"
