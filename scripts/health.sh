6c0709be3151971ee5f82623bbf6029183be4efd
jun <xodlf100@gmail.com>
2025-07-27 21:36:35 +0900

    fix #10 배포 스크립트 수정 - 3

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-
-# 현재 health.sh가 속해있는 경로를 찾는다.
 ABSDIR=$(dirname $ABSPATH)
-
-# 일종의 import, 해당 코드로 인해 health.sh에서 profile.sh의 여러 function 사용 가능
 source ${ABSDIR}/profile.sh
-
 source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
